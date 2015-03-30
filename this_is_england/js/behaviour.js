0fbd7f24df06f8c8c19c5fa6142122aa198f9a8b
minjin6 <paws.designs@yahoo.co.uk>
2015-03-30 21:10:54 +0100

    Added my project to Git

---
+++ this_is_england/js/behaviour.js
@@ -0,0 +1,17 @@
+$('#hambuger').click(function() {
+    $('nav').toggleClass('open');
+});
+
+/*
+$.getJSON('http://martinjc.github.io/UK-GeoJSON/json/eng/topo_eer.json', {}, function(){
+
+});
+
+var images = {
+    'London':[],
+    'West Midland':[],
+    'South West':[],
+    'South':[]
+}
+*/
+
