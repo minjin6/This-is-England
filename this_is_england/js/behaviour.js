3b869672d4a5c6b04d7e1ccbdb76adbf0810628c
minjin6 <paws.designs@yahoo.co.uk>
2015-04-01 07:31:13 +0100

    These are some updates made to the code.
    I add an image to the about page and also clean up the header on the
    contact page

--- this_is_england/js/behaviour.js
+++ this_is_england/js/behaviour.js
@@ -2,6 +2,10 @@ $('#hambuger').click(function() {
     $('nav').toggleClass('open');
 });
 
+$('#london').hover(function() {
+    $('#london').toggleClass('rollOver');
+});
+
 /*
 $.getJSON('http://martinjc.github.io/UK-GeoJSON/json/eng/topo_eer.json', {}, function(){
 
