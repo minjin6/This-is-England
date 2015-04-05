c681ce41f86f1802c7500450ac84eed453ac2f0d
minjin6 <paws.designs@yahoo.co.uk>
2015-04-05 20:30:52 +0100

    Added the hover states to the map.
    Also tested out the click functionality in the JavaScript

--- this_is_england/js/behaviour.js
+++ this_is_england/js/behaviour.js
@@ -1,21 +1,25 @@
+//Regions
+var london = $('#london');
+var westMidlands = $('#westMidlands');
+var eastMidlands = $('#eastMidlands');
+var eastEngland = $('#eastEngland');
+
+//collection of my images in the various locations
+var images = {
+    london: ['met','tower'],
+    westMidlands: [23,4,35,],
+    eastEngland: [true],
+    eastMidlands: [23,'Why',0.35],
+};
+
+//navigation button controls
 $('#hambuger').click(function() {
     $('nav').toggleClass('open');
 });
 
-$('#london').hover(function() {
-    $('#london').toggleClass('rollOver');
+//image filtering
+images.click(function() {
+    console.log('hello');
 });
 
-/*
-$.getJSON('http://martinjc.github.io/UK-GeoJSON/json/eng/topo_eer.json', {}, function(){
-
-});
-
-var images = {
-    'London':[],
-    'West Midland':[],
-    'South West':[],
-    'South':[]
-}
-*/
 
